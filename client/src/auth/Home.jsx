16c003c73b133cd866f28658d2d6696cc39ef3c1
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-13 01:11:58 +0800

    setup react tailwind && router

---
+++ client/src/auth/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center mt-4">
+      <h1 className="text-3xl text-red-400">
+        Welcome to Personal Finance Tracker
+      </h1>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
