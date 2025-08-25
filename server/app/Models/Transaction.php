3ad2c360433ac56c2c184ca6687f00840c6121cc
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-25 21:31:46 +0800

    setup relations

--- server/app/Models/Transaction.php
+++ server/app/Models/Transaction.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Transaction extends Model
 {
-    
+    protected $fillable = [
+        'user_id',
+        'title',
+        'type',
+        'amount',
+        'date',
+        'description',
+    ];
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
