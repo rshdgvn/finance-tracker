b8d583b8dabc237a32bca7129f58e14d1d6aed69
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-26 01:11:43 +0800

    create auth context to access tokens

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { token, setToken } = useAuth();
 
-export default Dashboard
+  const handleLogout = async () => {
+    try {
+      const res = await fetch("/api/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (res.ok) {
+        nav('/')
+        localStorage.removeItem("token");
+        setToken(null);
+        console.log("Logged out successfully");
+      } else {
+        console.error("Logout failed:", await res.json());
+      }
+    } catch (error) {
+      console.log("token: ", token);
+      console.error("Logout error:", error);
+    }
+  };
+
+  return (
+    <>
+      <div>Dashboard</div>
+      <button onClick={handleLogout}>Log Out</button>
+    </>
+  );
+};
+
+export default Dashboard;
