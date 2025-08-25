4e4457ae0ea95c059d1bec259ef7f648729d95b0
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-26 04:51:27 +0800

    transaction index

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
   const { token, setToken, user } = useAuth();
@@ -33,27 +34,7 @@ const Dashboard = () => {
 
   return (
     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
-      <nav className="bg-white shadow px-6 py-4 flex justify-between items-center">
-        <div className="flex items-center gap-6">
-          <button
-            onClick={() => nav("/transactions")}
-            className="text-gray-700 font-medium hover:text-blue-600 transition"
-          >
-            Transactions
-          </button>
-        </div>
-
-        <div className="flex items-center gap-4">
-          {user && <span className="text-gray-600">Hi, {user.name}</span>}
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
-          >
-            Log Out
-          </button>
-        </div>
-      </nav>
-
+      <Navbar handleLogout={handleLogout} user={user} />
       <main className="flex-1 p-6">
         <h1 className="text-2xl font-bold text-gray-800 mb-4">Dashboard</h1>
         <div className="bg-white p-6 rounded-xl shadow">
--- server/app/Http/Controllers/TransactionController.php
+++ server/app/Http/Controllers/TransactionController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    public function index() 
+    public function index()
     {
+        $user = Auth::user(); 
+
         return response()->json([
-            "Transacation" => Transaction::all(), 
-        ], 201);
+            "transactions" => Transaction::where('user_id', $user->id)->get(),
+        ], 200);
     }
 }
