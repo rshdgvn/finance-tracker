df4730a9770f6a31b8dcef9448093b976ca932c6
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-25 20:49:22 +0800

    added migrations

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,3 +12,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
